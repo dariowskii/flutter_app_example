b5289445796592a366db5d97da153f59cc9d97e5
Dario Varriale <dario.varriale@atobit.it>
2024-11-15 16:32:17 +0100

    Add TabBarView

--- lib/features/home_page_feature/presentation/home_page.dart
+++ lib/features/home_page_feature/presentation/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sisal/features/external_link_tab_feature/presentation/external_link_tab.dart';
+import 'package:sisal/features/list_tab_feature/presentation/list_tab.dart';
+import 'package:sisal/features/photo_tab_feature/presentation/photo_tab_feature.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,15 +10,51 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  final _tabs = [
+    const ListTab(),
+    const ExternalLinkTab(),
+    const PhotoTabFeature(),
+  ];
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(
+      length: _tabs.length,
+      vsync: this,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sisal'),
       ),
-      body: const Center(
-        child: Text('This is the home page'),
+      body: TabBarView(
+        controller: _tabController,
+        children: _tabs,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'List',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.link),
+            label: 'External Link',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo),
+            label: 'Photo',
+          ),
+        ],
       ),
     );
   }
